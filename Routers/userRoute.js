7bc6a9d9a6ef7de66446326e31198544e58412e7
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-10-08 00:00:24 +0530

    userRoute added

---
+++ Routers/userRoute.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const authRoute = express.Router();
+
+
+//const upload = require('../Middleware/multer.middleware');
+
+//const {isLoggedIn, authorizedRoles} = require('../Middleware/auth.middleware');
+
+const {register,
+       login,
+       logout,
+       getprofile,
+       forgotPassword,
+       resetPassword,
+       changePassword,
+updateUser,
+getAllUser} = require('../Controllers/userController')
+
+
+//authRoute.post('/register', upload.single("avatar"), register);
+// authRoute.post('/login', login);
+// authRoute.post('/logout', logout);
+// authRoute.get('/me', 
+//                //isLoggedIn, 
+//                getprofile);
+// authRoute.post("/reset", 
+//                forgotPassword);
+// authRoute.post("/reset/:resetToken", 
+//                 resetPassword);
+// authRoute.post('/change-password', 
+//                //isLoggedIn, 
+//                changePassword);
+// authRoute.put('/update',
+//                  isLoggedIn,  
+//                  //upload.single("avatar"), 
+//                  updateUser);
+
+// authRoute.get('/getAll', 
+//              // isLoggedIn,  
+//             // authorizedRoles('ADMIN'), 
+//              getAllUser);
+
+
+
+
+module.exports = authRoute;
