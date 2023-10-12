f526bab84251b89ce2825da8cfca3f6b07d88889
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-10-12 19:00:49 +0530

    app.js added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true })); //Third-party middleware
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true })); //Third-party middleware
 
 app.use(cookieParser());   // Third-party middleware
 
